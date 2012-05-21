a4b1b4ad6b63e0e1443fbe58815541dcdf4a1e7e
Bob Nystrom <robert@stuffwithstuff.com>
2012-05-21 13:08:49 -0700

    Initial commit.

---
+++ kittens.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/** A simple library for getting URLs to pictures of kittens. */
+#library('kittens');
+
+final numKittens = 5;
+
+getKitten(int index) {
+  return 'http://stuffwithstuff.com/temp/kittens/${index + 1}.jpg';
+}
