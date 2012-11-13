94c323a7eeeac42505133d912e73bfdea8b92dbb
Seth Ladd <sethladd@sethladd-macbookpro.local>
2012-11-13 11:15:07 +0100

    update to m1 syntax

--- lib/kittens.dart
+++ lib/kittens.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /** A simple library for getting URLs to pictures of kittens. */
-#library('kittens');
+library kittens;
 
 final numKittens = 5;
 
